b08db86ccbbec24e54344757fa14fa84ea43f9dd
JonathanDefraiteur <jonathan.defraiteur@gmail.com>
2017-09-08 17:23:18 +0200

    Add RotationDAS

---
+++ Assets/SimonComponent/Scripts/ActionScripts/RotateDAS.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SimonComponent.ActionScripts
+{
+	public class RotateDAS : AbstractDAS
+	{
+		public Vector3 rotation;
+        
+		public override string GetActionScriptName()
+		{
+			return "Rotation";
+		}
+
+		public override void OnTurnOn(SimonDisplayer displayer)
+		{
+			displayer.transform.Rotate(rotation);
+		}
+
+		public override void OnTurnOff(SimonDisplayer displayer)
+		{
+			displayer.transform.Rotate(-rotation);
+		}
+	}
+
+	[CustomEditor(typeof(RotateDAS))]
+	public class RotateDASEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			RotateDAS scriptableObject = (RotateDAS) target;
+
+			scriptableObject.rotation = EditorGUILayout.Vector3Field("", scriptableObject.rotation);
+		}
+	}
+}
